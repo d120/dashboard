12cd42830569076c3ae49898f2650a3fdc47b852
Fabian Damken <fabian.damken@gmx.de>
2017-04-23 15:18:15 +0200

    Project cleanup to get started.

--- src/main/java/de/d120/dashboard/D120DashboardApplication.java
+++ src/main/java/de/d120/dashboard/D120DashboardApplication.java
@@ -3,10 +3,20 @@ package de.d120.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main class of the application.
+ *
+ * @author Fabian Damken
+ */
 @SpringBootApplication
 public class D120DashboardApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(D120DashboardApplication.class, args);
-	}
+    /**
+     * The main method.
+     *
+     * @param args
+     *            The CLI arguments.
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(D120DashboardApplication.class, args);
+    }
 }
