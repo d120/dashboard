6cff8610aab23dcb4b6f04588bf41371fd7749a0
Fabian Damken <fabian.damken@gmx.de>
2017-04-23 15:12:15 +0200

    Initial commit.

---
+++ src/main/java/de/d120/dashboard/D120DashboardApplication.java
@@ -0,0 +1,12 @@
+package de.d120.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D120DashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D120DashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/de/d120/dashboard/D120DashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package de.d120.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class D120DashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
